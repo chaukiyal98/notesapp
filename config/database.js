90488c239a4afeedf75da15bb52dedc771b6806e
unknown <chaukiyal98@gmail.com>
2019-01-08 14:34:47 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://mongodb://<dbuser>:<dbpassword>@ds127634.mlab.com:27634/mfirst-app'}
+  module.exports = {mongoURI: 'mongodb://mongodb://RadheyNaman:namanji9897177811//@ds127634.mlab.com:27634/mfirst-app'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
